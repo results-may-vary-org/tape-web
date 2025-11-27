25f592261e1b9e3831a25bc6700a901893f919d1
Bouteiller [a2n] Alan <a2n.dev@pm.me>
2025-11-27 10:32:33 +0100

    work on animation

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-window.addEventListener('scroll', () => {
-  const element = document.getElementById('header');
+document.addEventListener("DOMContentLoaded", (event) => {
+
+  // handle the shadow for the header
+  window.addEventListener('scroll', () => {
+    const element = document.getElementById('header');
+
+    if (window.scrollY >= 50) {
+      element.classList.add('blurred');
+    } else {
+      element.classList.remove('blurred');
+    }
+  });
+
+  // animate the text
+  const {animate, splitText, stagger} = anime; 
+
+  const elems = document.getElementsByClassName('anime');
   
-  if (window.scrollY >= 50) {
-    element.classList.add('blurred');
-  } else {
-    element.classList.remove('blurred');
-  }
+  console.log(elems)
+
+  Object.values(elems).forEach(element => {
+    splitText(element, {lines: { wrap: 'clip' }})
+    .addEffect(({ lines }) => animate(lines, {
+      y: [{ to: ['100%', '0%'] }],
+      duration: 750,
+      ease: 'out(3)',
+      delay: stagger(200),
+      loop: false,
+    }));
+  });
 });
