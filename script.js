03110a01e7ce2dbc1a3d3b814b03179d1181f4fa
Bouteiller [a2n] Alan <a2n.dev@pm.me>
2025-12-06 17:08:01 +0100

    update animation

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const { animate, svg, stagger, createTimeline } = anime;
+const { animate, svg, stagger, createTimeline, onScroll, splitText } = anime;
 
 const tl = createTimeline();
 
@@ -6,10 +6,81 @@ tl.label('start')
   .add(svg.createDrawable('.svanim'), {
     draw: ['0 0', '0 1'],
     ease: 'inOutQuad',
-    duration: 1800,
+    duration: 800,
     delay: stagger(100),
     loop: false
   }, 'start')
   .add(['.svanim'], {fill: '#292524', duration: 200})
-  .add(['.picture'], {opacity: [0, .5, 1]});
+  .add(['.picture'], {opacity: [0, 1]})
+  .call(() => document.getElementsByTagName('body')[0].classList.remove('ns'));
+
+const p = [...document.getElementsByTagName("p")];
+const btn = [...document.getElementsByTagName("button")];
+const footer = [...document.getElementsByTagName("h6")];
+const icon = [...document.getElementsByClassName("icon")];
+const debug = false;
+const duration = 560;
+
+if (p.length > 0) {
+  p.forEach(element => {
+    const { words } = splitText(element, {words: { wrap: 'clip' }});
+    animate(words, {
+      y: [100, 0],
+      opacity: [0, 1],
+      duration,
+      ease: 'out(3)',
+      delay: stagger(90), // between each words
+      autoplay: onScroll({
+        target: element.parentElement,
+        debug
+      })
+    });
+  });
+}
+
+if (icon.length > 0) {
+  icon.forEach(element => {
+    animate(element, {
+      y: [100, 0], // TODO do not work
+      opacity: [0, 1],
+      duration,
+      ease: 'out(3)',
+      autoplay: onScroll({
+        target: element.parentElement,
+        debug
+      })
+    });
+  });
+}
+
+if (btn.length > 0) {
+  btn.forEach(element => {
+    animate(element, {
+      y: [100, 0],
+      opacity: [0, 1],
+      duration,
+      ease: 'out(3)',
+      autoplay: onScroll({
+        target: element.parentElement,
+        debug
+      })
+    });
+  });
+}
+
+if (footer.length > 0) {
+  footer.forEach(element => {
+    animate(element, {
+      y: [100, 0],
+      opacity: [0, 1],
+      duration,
+      ease: 'out(3)',
+      delay: stagger(90),
+      autoplay: onScroll({
+        target: element.parentElement,
+        debug
+      })
+    });
+  });
+}
 
